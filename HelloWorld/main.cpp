6c6923539dd6b8928443849bab6db6582db385d0
Daria <dariafalina24@gmail.com>
2025-02-14 17:27:50 +0500

    Добавлена новая функция

---
+++ HelloWorld/main.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <vector>
+#include <random>
+#include <string>   
+#include <sstream> 
+#include <cctype> 
+#include <algorithm>
+#include <set>
+
+using namespace std;
+
+int main()
+{
+    setlocale( 0, "" );
+    
+    cout << "Hello воробей ";
+
+    return 0;
+}
